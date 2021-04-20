6dce1561fc5947b368ae29d175f0fe79f91c30d6
Mateo Sam <sammateo4@gmail.com>
2021-04-20 17:10:43 -0400

    add: width

--- canvas.js
+++ canvas.js
@@ -1,21 +1,26 @@
-let canvas = document.querySelector('canvas');
+let canvas = document.querySelector('.canvas');
 let colorPicker = document.querySelector('#colorPicker')
+let strokeWidth = document.querySelector('#width')
 canvas.width = window.innerWidth * .8;
 canvas.height = window.innerHeight * .8;
 canvas.style.display ='block'
 canvas.style.margin = 'auto'
-
+strokeWidth.value = 1;
 let c = canvas.getContext('2d');
 let check = false
 c.beginPath();
+
+c.setLineDash([0]);
+
 canvas.addEventListener('mousedown',function(e){
     check = true;
+    c.strokeStyle = colorPicker.value
+    c.lineWidth = strokeWidth.value
     let x = e.offsetX
     let y = e.offsetY
     c.moveTo(x,y);
-    canvas.addEventListener('mousemove',function(e){
-        c.strokeStyle = colorPicker.value
-
+    canvas.addEventListener('mousemove',function(e){    
+        colorPicker.style.border = '2px solid '+ colorPicker.value;
         if(!check){
             return;
         }
@@ -28,6 +33,7 @@ canvas.addEventListener('mousedown',function(e){
     canvas.addEventListener('mouseup',function(e){
         check =false;
         c.beginPath()
+        console.log(c)
     })
 
 })
@@ -35,16 +41,17 @@ canvas.addEventListener('mousedown',function(e){
 
 canvas.addEventListener('touchstart',function(e){
     check = true;
+    c.strokeStyle = colorPicker.value
+    c.lineWidth = strokeWidth.value
     let x = e.touches[0].clientX
     let y = e.touches[0].clientY
     c.moveTo(x,y);
     canvas.addEventListener('touchmove',function(e){
-        c.strokeStyle = colorPicker.value
-
+        colorPicker.style.border = '2px solid '+ colorPicker.value;
         if(!check){
             return;
         }
-        let x = e.touches[0].clientX
+        let x = e.touches[0].clientX 
         let y = e.touches[0].clientY
         c.lineTo(x,y);
         c.stroke();
